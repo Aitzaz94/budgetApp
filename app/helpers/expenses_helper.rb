2e096c3773b6fbc637dbd6d957f6baedca4466bb
Aitzaz94 <32670716+Aitzaz94@users.noreply.github.com>
2024-01-31 23:52:04 +0500

    added expenses per day table view, displayed total per day, total expenses per month

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,11 @@
 module ExpensesHelper
+
+    def total_amount_for_day(expenses)
+        expenses.sum(&:amount).round(2)
+    end
+
+    def total_amount_for_month(expenses)
+        expenses.sum(&:amount).round(2)
+    end
+
 end
