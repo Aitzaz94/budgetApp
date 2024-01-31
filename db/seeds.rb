0518d319f264f9b5e7bf5d07843262353bad524e
Aitzaz94 <32670716+Aitzaz94@users.noreply.github.com>
2024-02-01 01:23:32 +0500

    added month view selection and seeded the data for view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+(1..12).each do |month|
+    Expense.create(
+        name: "Expense for #{ Date::MONTHNAMES[month] }",
+        amount: rand(10000),
+        date: Date.new(rand(2020..2024), month, rand(1..29))
+    )
+end
